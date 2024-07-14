33c4d68720485f5a6ee055a5931923bd1576fe9a
Chaitanya Bhagwat <chai.bhagwat@gmail.com>
2024-07-14 19:59:40 +0530

    Add build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+rm -rf public/jupy_lite
+
+# Build the extention project first
+cd jupyter-lab-ext
+
+pip install -ve .
+pip install jupyterlab
+
+jlpm run build
+
+cd ../public
+
+# Create static assets for the jupyter lab ext
+jupyter lite build --output-dir jupy_lite
+
+# Build the UI
+yarn --frozen-lockfile install; pnpm run build
