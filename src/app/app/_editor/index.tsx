3ce9565c7c667287990c671f3dc562ad99a822d9
Chaitanya Bhagwat <chai.bhagwat@gmail.com>
2024-07-14 17:02:15 +0530

    Increase code editor and Jupyter width

--- src/app/app/_editor/index.tsx
+++ src/app/app/_editor/index.tsx
@@ -69,13 +69,13 @@ export function Editor() {
       <Sidebar />
       <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
         <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-8">
-          <div className="col-span-5">
+          <div className="col-span-3">
             <div className="mb-4">
               <ActionContainer />
             </div>
             <DataViewerContainer />
           </div>
-          <div className="col-span-3">
+          <div className="col-span-5">
             <Tabs defaultValue="jupyter">
               <TabsList>
                 <TabsTrigger value="native">Native</TabsTrigger>
