e049e357e292bee97c7a80990a5b60c42043bfa5
Chaitanya Bhagwat <chai.bhagwat@gmail.com>
2024-08-10 17:39:53 +0530

    Remove workbooks table

--- packages/model/src/dbTables.ts
+++ packages/model/src/dbTables.ts
@@ -1,5 +1,4 @@
 import { Tables } from "./database.types";
 
 export type DBProject = Tables<"projects">;
-export type DBWorkbook = Tables<"workbooks">;
 export type DBWorkbookData = Tables<"workbook_data">;
