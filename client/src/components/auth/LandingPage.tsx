a32d5581dfc66ed83f80b4589e0406435cffbcdd
vikram-athalye <vidhat1996@gmail.com>
2024-09-10 04:08:39 +0530

    minor ui fixes

--- client/src/components/auth/LandingPage.tsx
+++ client/src/components/auth/LandingPage.tsx
@@ -2,8 +2,6 @@ import React, { useRef } from "react"
 import { Link } from "@tanstack/react-router"
 import { motion, useInView } from "framer-motion"
 
-import { ArrowRight } from "lucide-react"
-
 import HowItWorks from "./HowItWorks"
 import aiAnimation from "/Users/vikram/Documents/CODE/soupknit/client/src/assets/landing-page/AI.gif"
 import lookingForInsights from "/Users/vikram/Documents/CODE/soupknit/client/src/assets/landing-page/lookingForInsights.gif"
@@ -11,13 +9,23 @@ import soupKnitArt from "/Users/vikram/Documents/CODE/soupknit/client/src/assets
 import aiTools from "/Users/vikram/Documents/CODE/soupknit/client/src/assets/landing-page/tools.gif"
 import { Button } from "@/components/ui/button"
 
+const textRaiseAttributes = {
+  hidden: { opacity: 0, y: 60, scale: 0.9 },
+  visible: { opacity: 1, y: 0, scale: 1 },
+}
+
+const buttonRaiseAttributes = {
+  hidden: { opacity: 0, y: 60, scale: 0.9 },
+  visible: { opacity: 1, y: 0, scale: 1 },
+}
+
 export function LandingPage() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-[#fef2f225] to-[#fef3c720]">
       <header className="container mx-auto px-4 py-8">
         <nav className="flex items-center justify-between">
-          <div className="text-2xl font-bold" style={{ color: "#422c75" }}>
-            Soupknit
+          <div className="text-2xl font-bold" style={{ color: "#46278d" }}>
+            SoupKnit
           </div>
           <div className="space-x-4">
             <Link to="/signin">
@@ -31,42 +39,52 @@ export function LandingPage() {
       </header>
 
       <main className="container mx-auto px-16 py-16">
-        <section className="flex text-left">
-          <div className="mr-8 flex-grow gap-y-2">
-            <h1 className="mb-6 font-headline text-6xl font-semibold leading-none text-gray-700 dark:text-blue-100">
-              <span>No-Code</span>
-              <br />
-              <span>Machine Learning</span>
-              <br />
-              <span className="">for Everyone</span>
-            </h1>
-            <p className="mb-8 max-w-2xl text-xl text-gray-600 dark:text-gray-400">
-              <span>Build, train, and deploy machine learning models</span>
-              <br />
-              <span>without writing a single line of code.</span>
+        <motion.div
+          className="pl-20"
+          initial={{ y: 20, opacity: 0.2 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.8, ease: "easeInOut" }}
+        >
+          <section className="flex text-left">
+            <div>
+              <h1 className="mb-6 font-headline text-6xl font-semibold leading-none text-gray-700 dark:text-blue-100">
+                <span>No-Code</span>
+                <br />
+                <span>Machine Learning</span>
+                <br />
+                <span>for </span>
+                <span className="text-[#46278d]">Everyone</span>
+              </h1>
+              <p className="mb-8 max-w-2xl text-xl text-gray-600 dark:text-gray-400">
+                <span>Build, train, and deploy machine learning models</span>
+                <br />
+                <span>without writing a single line of code.</span>
+              </p>
+            </div>
+            <div className="hero-image-container pl-40">
+              <img
+                src={soupKnitArt}
+                alt="soupKnit Art"
+                className="h-auto w-full sm:max-w-xs md:max-w-md lg:max-w-md xl:max-w-md"
+                style={{ filter: "grayscale(50%)" }}
+              />
+            </div>
+          </section>
+          <section className="mb-20">
+            <Link to="/signup">
+              <Button size="lg" className="gap-2 px-8 text-lg">
+                Get Started. It's free<span>→</span>
+              </Button>
+            </Link>
+            <p className="mt-4 max-w-2xl text-sm text-gray-600 dark:text-gray-400">
+              Built for Data Scientists, Enthusiasts, and noobs.
             </p>
-          </div>
-          <img
-            src={soupKnitArt}
-            alt="soupKnit Art"
-            className="h-auto w-full sm:max-w-xs md:max-w-md lg:max-w-lg xl:max-w-xl"
-            style={{ filter: "grayscale(50%)" }}
-          />
-        </section>
-        <section className="mb-20">
-          <Link to="/signup">
-            <Button size="lg" className="px-8 py-6 text-lg">
-              Get Started. It's free <ArrowRight className="ml-2" />
-            </Button>
-          </Link>
-          <p className="mt-4 max-w-2xl text-sm text-gray-600 dark:text-gray-400">
-            Built for Data Scientists, Enthusiasts, and noobs!
-          </p>
-        </section>
+          </section>
+        </motion.div>
 
-        <div className="flex w-full flex-col items-center bg-[#FFFFFF] p-4">
+        <div className="flex w-full flex-col items-center rounded-2xl bg-[#FFFFFF] p-4">
           <h1 className="my-6 text-center font-headline text-4xl font-semibold text-gray-700 dark:text-blue-100">
-            <span>Need data insights, </span>
+            <span>Need data insights? </span>
             <br />
             <span>but don’t have the tools or the team?</span>
             <br />
@@ -89,30 +107,36 @@ export function LandingPage() {
                   "Want to impress your clients using AI, but don’t have the tools ?",
               },
             ].map((feature, index) => (
-              <FeatureCard index={index} description={feature.description}>
-                <img
-                  src={feature.gif}
-                  alt="GIF"
-                  className="mb-4 h-12 w-12"
-                  style={{
-                    height: "200px",
-                    width: "auto",
-                    filter: "grayscale(40%)",
-                  }}
-                />{" "}
-              </FeatureCard>
+              <FeatureCard
+                index={index}
+                description={feature.description}
+                gif={feature.gif}
+              />
             ))}
           </section>
 
-          <h2 className="pb-6 text-xl font-semibold">
-            Soupknit is the no code AI solution you are looking for.
-          </h2>
+          <motion.h2
+            className="pb-6 text-xl font-semibold"
+            variants={textRaiseAttributes}
+            initial="hidden"
+            animate="visible"
+            transition={{ delay: 4.5, duration: 0.5, ease: "easeOut" }}
+          >
+            Soupknit is the no code AI solution you are looking for !
+          </motion.h2>
 
-          <Link to="/signup">
-            <Button size="sm" className="mb-5 px-8 py-6 text-sm">
-              Get Started. It's free <ArrowRight className="ml-2" />
-            </Button>
-          </Link>
+          <motion.div
+            variants={buttonRaiseAttributes}
+            initial="hidden"
+            animate="visible"
+            transition={{ delay: 5.5, duration: 0.5, ease: "easeOut" }}
+          >
+            <Link to="/signup">
+              <Button size="sm" className="mb-5 gap-2 px-8 py-6 text-sm">
+                Get Started. It's free<span>→</span>
+              </Button>
+            </Link>
+          </motion.div>
         </div>
         <div className="soupknit-steps-container">
           <h2
@@ -122,13 +146,13 @@ export function LandingPage() {
               justifyContent: "center",
             }}
           >
-            How SoupKnit works
+            How SoupKnit works?
           </h2>
           {<HowItWorks />}
         </div>
       </main>
 
-      <footer className="mt-16 bg-gray-800 py-8 text-white">
+      <footer className="mt-8 bg-black py-8 text-white">
         <div className="container mx-auto px-4 text-center">
           <p>&copy; 2024 Soupknit. All rights reserved.</p>
         </div>
@@ -138,57 +162,42 @@ export function LandingPage() {
 }
 
 function FeatureCard({
-  title,
+  gif,
   description,
-  children,
-  className,
   index,
 }: {
   title?: string
   description?: string
-  children?: React.ReactNode
+  gif?: String
   className?: string
   index: number
 }) {
   const ref = useRef(null)
-  const isInView = useInView(ref, { once: true, amount: 0.5 })
+  const isInView = useInView(ref, { once: true, amount: 0 })
 
   return (
-    <div ref={ref} className="flex flex-col items-center justify-center gap-4">
-      <motion.div
-        initial={{ opacity: 0, y: 0, scale: 0 }}
-        animate={
-          isInView
-            ? { opacity: 1, y: 0, scale: 1 }
-            : { opacity: 0, y: 300, scale: 0.7 }
-        }
-        transition={{
-          duration: 1.1,
-          delay: index * 1.5,
-          ease: "easeIn",
-          type: "spring",
-          stiffness: 35,
-          damping: 10,
-        }}
-      >
-        {children}
-      </motion.div>
-
-      <motion.p
-        className="max-w-xs text-center text-gray-600 dark:text-gray-200"
-        initial={{ opacity: 0 }}
-        animate={isInView ? { opacity: 1 } : { opacity: 0, y: 60 }}
-        transition={{
-          duration: 1.1,
-          delay: index * 1.5,
-          ease: "easeOut",
-          type: "spring",
-          stiffness: 100,
-          damping: 10,
-        }}
-      >
+    <motion.div
+      ref={ref}
+      className="flex flex-col items-center justify-center gap-4"
+      initial={{ opacity: 0, y: 200, scale: 0.8 }}
+      animate={isInView ? { opacity: 1, y: 0, scale: 1 } : null}
+      transition={{
+        duration: 1.1,
+        ease: "easeIn",
+        type: "spring",
+        stiffness: 35,
+        damping: 10,
+      }}
+    >
+      <img
+        className="feature-gif w-60"
+        src={gif}
+        alt="feature gif"
+        style={{ filter: "grayscale(20%)" }}
+      ></img>
+      <p className="max-w-xs text-center text-gray-600 dark:text-gray-200">
         {description}
-      </motion.p>
-    </div>
+      </p>
+    </motion.div>
   )
 }
